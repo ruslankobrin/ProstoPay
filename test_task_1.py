3ab3f0844530dd652a23c5e773af48871fc0ed7d
ruslankobrynchuk <ruslankobrynchuk@gmail.com>
2024-04-18 17:11:39 +0300

    add unittest

---
+++ test_task_1.py
@@ -0,0 +1,25 @@
+import unittest
+
+from task_1 import HashMap
+
+
+class MyTestCase(unittest.TestCase):
+    def test_hashmap(self):
+        hashmap = HashMap()
+
+        hashmap.put("apple", 10)
+        self.assertEqual(hashmap.get("apple"), 10)
+
+        hashmap.put("apple", 20)
+        self.assertEqual(hashmap.get("apple"), 20)
+
+        self.assertEqual(hashmap.get("banana"), None)
+
+        hashmap.put("lemon", 30)
+        hashmap.put("melon", 40)
+        self.assertEqual(hashmap.get("lemon"), 30)
+        self.assertEqual(hashmap.get("melon"), 40)
+
+
+if __name__ == '__main__':
+    unittest.main()
