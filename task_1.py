5232792c87f82c46fa6c5df28ae459313628d068
ruslankobrynchuk <ruslankobrynchuk@gmail.com>
2024-04-18 17:04:36 +0300

    add comments

--- task_1.py
+++ task_1.py
@@ -1,12 +1,36 @@
 class HashMap:
     def __init__(self, size=10):
+        """
+        Initializes the HashMap with an optional initial size.
+
+        Args:
+            size (int): The initial size of the HashMap (default is 10).
+        """
         self.size = size
         self.map = [None] * size
 
     def _hash(self, key):
+        """
+        Generates a hash code for the given key.
+
+        Args:
+            key: The key to be hashed.
+
+        Returns:
+            int: The hash code for the key.
+        """
         return hash(key) % self.size
 
     def put(self, key, value):
+        """
+        Inserts a key-value pair into the HashMap.
+
+        If a key already exists, its value will be updated.
+
+        Args:
+            key: The key of the entry to be inserted or updated.
+            value: The value associated with the key.
+        """
         index = self._hash(key)
         if self.map[index] is None:
             self.map[index] = [(key, value)]
@@ -18,6 +42,15 @@ class HashMap:
             self.map[index].append((key, value))
 
     def get(self, key):
+        """
+        Retrieves the value associated with the given key from the HashMap.
+
+        Args:
+            key: The key whose associated value is to be retrieved.
+
+        Returns:
+            The value associated with the key, or None if the key is not found.
+        """
         index = self._hash(key)
         if self.map[index] is None:
             return None
